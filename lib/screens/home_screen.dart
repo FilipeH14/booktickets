895cf2385eddf3e3789ca01cf4a46c946b2b174c
Filipe Haniel <filipe.hanielloper@gmail.com>
2022-08-03 00:38:11 -0300

    ajuste de elementos do cabeçalho na home

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -10,15 +10,26 @@ class HomeScreen extends StatelessWidget {
       body: ListView(
         children: [
           Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Column(
               children: [
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    const Text('Good Morning'),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text('Good Morning'),
+                        Text('Book Tickets'),
+                      ],
+                    ),
                     Container(
+                      height: 50,
+                      width: 50,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
                         image: const DecorationImage(
+                          fit: BoxFit.fitHeight,
                           image: AssetImage('assets/images/img_1.png'),
                         ),
                       ),
