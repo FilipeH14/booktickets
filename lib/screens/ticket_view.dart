897f09379c4238d09556533b13c89753ca8da841
Filipe Haniel <filipe.hanielloper@gmail.com>
2022-08-04 23:36:03 -0300

    desenhando primeira parte da widget viagem

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -1,5 +1,6 @@
 import 'package:booktickets/utils/app_layout.dart';
 import 'package:booktickets/utils/app_styles.dart';
+import 'package:booktickets/widgets/tick_container.dart';
 import 'package:flutter/material.dart';
 
 class TicketView extends StatelessWidget {
@@ -12,7 +13,7 @@ class TicketView extends StatelessWidget {
       width: size.width,
       height: 200,
       child: Container(
-        margin: const EdgeInsets.only(left: 16),
+        margin: const EdgeInsets.symmetric(horizontal: 16),
         child: Column(
           children: [
             Container(
@@ -33,7 +34,26 @@ class TicketView extends StatelessWidget {
                         style:
                             Styles.headLineStyle3.copyWith(color: Colors.white),
                       ),
-                      const Spacer(),
+                      Expanded(child: Container()),
+                      const TickContainer(),
+                      Expanded(
+                        child: SizedBox(
+                          height: 24,
+                          child: LayoutBuilder(
+                            builder: (BuildContext context, BoxConstraints constraints) => Flex(
+                              direction: Axis.horizontal,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: List.generate(
+                                (constraints.constrainWidth()/6).floor(),
+                                (index) => const Text('-',
+                                    style: TextStyle(color: Colors.white)),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const TickContainer(),
+                      Expanded(child: Container()),
                       Text(
                         'London',
                         style:
