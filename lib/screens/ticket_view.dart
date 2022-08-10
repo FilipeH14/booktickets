3feb4461fea59c6d9f0c62a38ce4fbc5a150d524
Filipe Haniel <filipe.hanielloper@gmail.com>
2022-08-10 02:09:32 -0300

    primeira parte do card de viagem concluida

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -37,19 +37,42 @@ class TicketView extends StatelessWidget {
                       Expanded(child: Container()),
                       const TickContainer(),
                       Expanded(
-                        child: SizedBox(
-                          height: 24,
-                          child: LayoutBuilder(
-                            builder: (BuildContext context, BoxConstraints constraints) => Flex(
-                              direction: Axis.horizontal,
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: List.generate(
-                                (constraints.constrainWidth()/6).floor(),
-                                (index) => const Text('-',
-                                    style: TextStyle(color: Colors.white)),
+                        child: Stack(
+                          children: [
+                            SizedBox(
+                              height: 24,
+                              child: LayoutBuilder(
+                                builder: (BuildContext context,
+                                        BoxConstraints constraints) =>
+                                    Flex(
+                                  direction: Axis.horizontal,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: List.generate(
+                                    (constraints.constrainWidth() / 6).floor(),
+                                    (index) => const SizedBox(
+                                      width: 3,
+                                      height: 1,
+                                      child: DecoratedBox(
+                                        decoration:
+                                            BoxDecoration(color: Colors.white),
+                                      ),
+                                    ),
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
+                            Center(
+                              child: Transform.rotate(
+                                angle: 1.5,
+                                child: const Icon(
+                                  Icons.local_airport_rounded,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                       const TickContainer(),
